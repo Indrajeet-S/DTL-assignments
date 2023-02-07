6d499c8109090bb5d6e0b88bbe3950eaafa20c19
Indrajeet Shelake <indravshelake95@gmail.com>
2023-02-07 15:58:43 +0530

    if-else condition

---
+++ ifelse.sh
@@ -0,0 +1,15 @@
+mks=91
+if [ $mks -ge 35 ]
+then
+        echo "You are passed"
+else
+        echo "You failed"
+fi
+
+str="build"
+if [ $str != "build" ]
+then
+        echo "You are not on right path"
+else
+        echo "You are on right path"
+fi
