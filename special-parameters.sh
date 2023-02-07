9ee7a9937a4b2302ec85723493b72753148db343
Indrajeet Shelake <indravshelake95@gmail.com>
2023-02-07 16:56:56 +0530

    special parameters info

---
+++ special-parameters.sh
@@ -0,0 +1,19 @@
+echo
+echo $# Tells the total number of flags
+echo
+echo $* Represents all args as single string
+echo
+echo $- current option flag
+echo
+echo $$ is the PID of the shell
+echo
+echo $@ All args are represented as array
+echo
+echo $! is the PID of the last executed BG command
+echo
+echo $a is the script name
+echo
+echo $_ final arg of last executed Foreground command
+echo
+echo $? Represents the exit code of the last command
+echo
